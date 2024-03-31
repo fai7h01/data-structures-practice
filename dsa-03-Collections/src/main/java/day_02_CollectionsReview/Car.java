f95748966900caf51b256b40a8722fc886e6c14f
fai7h01 <l.varsimashvil10@gmail.com>
2024-03-31 19:19:22 +0400

    Set

---
+++ dsa-03-Collections/src/main/java/day_02_CollectionsReview/Car.java
@@ -0,0 +1,12 @@
+package day_02_CollectionsReview;
+
+public class Car {
+    int id;
+    String make;
+
+    public Car(int id, String make) {
+        this.id = id;
+        this.make = make;
+    }
+
+}
---
+++ dsa-03-Collections/src/main/java/day_02_CollectionsReview/SetTest.java
@@ -0,0 +1,18 @@
+package day_02_CollectionsReview;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetTest {
+    public static void main(String[] args) {
+
+        Set<Car> set = new HashSet<>();
+
+        Car car1 = new Car(1,"Honda");
+        set.add(car1);
+        Car car2 = new Car(1, "Honda");
+        set.add(car2);
+
+
+    }
+}
