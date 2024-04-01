933a08ec6aad93cc8ec945b5972de558d4fff986
fai7h01 <l.varsimashvil10@gmail.com>
2024-04-01 20:50:11 +0400

    ArrayList

--- dsa-03-Collections/src/main/java/ArrayListReview.java
+++ dsa-03-Collections/src/main/java/ArrayListReview.java
@@ -5,16 +5,27 @@ public class ArrayListReview {
         // Create ArrayList and a class
         List<Student> students = new ArrayList<>();
 
-// Add elements to ArrayList
+        // Add elements to ArrayList
+        students.add(new Student(1,"Melissa"));
+        students.add(new Student(2,"Julian"));
+        students.add(new Student(3,"Selena"));
+        students.add(new Student(4,"Yin"));
 
 
         // Iteration on ArrayLists
         // 1. For Loop with get(index)
-
+        System.out.println("Printing with legacy for-loop.......");
+        for (int i = 0; i < students.size(); i++) {
+            System.out.println(students.get(i));
+        }
 
         // 2. Iterator
         // Forward Iteration
-
+        System.out.println("Printing with iterator.......");
+        Iterator<Student> iterator = students.iterator();
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
 
         // Backwards Iteration
 
