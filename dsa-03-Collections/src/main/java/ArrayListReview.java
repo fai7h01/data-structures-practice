af06d62805930a2ce84c6bdc7798f9b6b5b7c879
fai7h01 <l.varsimashvil10@gmail.com>
2024-04-01 21:04:28 +0400

    ArrayList

--- dsa-03-Collections/src/main/java/ArrayListReview.java
+++ dsa-03-Collections/src/main/java/ArrayListReview.java
@@ -22,23 +22,55 @@ public class ArrayListReview {
         // 2. Iterator
         // Forward Iteration
         System.out.println("Printing with iterator.......");
-        Iterator<Student> iterator = students.iterator();
+        Iterator<Student> iterator = students.listIterator();
         while (iterator.hasNext()){
             System.out.println(iterator.next());
         }
 
         // Backwards Iteration
+        System.out.println("Printing backwards with iterator.......");
+        while(((ListIterator<?>) iterator).hasPrevious()){
+            System.out.println(((ListIterator<?>) iterator).previous());
+        }
 
+        // 3. for each
+        System.out.println("Printing with for each loop.......");
 
-        // 3. for each loop
+        for (Student each : students) {
+            System.out.println(each);
+        }
 
         // 4. Lambda
+        System.out.println("Printing with forEach method.......");
+        students.forEach(System.out::println);
 
 
         // Sorting Elements in List using comparator interface
+        System.out.println("Sorting with comparator interface descending order");
+        Collections.sort(students, new SortByDescending());
+        System.out.println(students);
+
+        Collections.sort(students, new SortByNameDescending());
+        System.out.println(students);
 
 
     }
     //  implementations of  Comparator
+
+    static class SortByDescending implements Comparator<Student> {
+
+        @Override
+        public int compare(Student o1, Student o2) {
+            return o2.id - o1.id;
+        }
+    }
+
+    static class SortByNameDescending implements Comparator<Student> {
+
+        @Override
+        public int compare(Student o1, Student o2) {
+            return o2.name.compareToIgnoreCase(o1.name);
+        }
+    }
 
 }
