57f2e162127207d39670d9a24daff284ab1eca4a
fai7h01 <l.varsimashvil10@gmail.com>
2024-04-02 20:43:24 +0400

    Map

--- dsa-03-Collections/src/main/java/MapReview.java
+++ dsa-03-Collections/src/main/java/MapReview.java
@@ -9,10 +9,28 @@ public class MapReview {
         studentsMap.put(1, "Jack");
         studentsMap.put(2, "Julian");
 
+        String str = "JJava_Developer";
+        System.out.println(findFirstNonRepeatingChar(str));
+
 
     }
 
     public static Character findFirstNonRepeatingChar(String str){
+        Map<Character,Integer> map = new HashMap<>();
+
+        for (char c : str.toCharArray()) {
+            if (map.containsKey(c)){
+                map.put(c, map.getOrDefault(c, 0) + 1);
+            }else{
+                map.put(c, 1);
+            }
+        }
+
+        for (char c : str.toCharArray()) {
+            if (map.get(c) == 1){
+                return c;
+            }
+        }
 
         return null;
     }
