997308ea7091d580adf741cd2dfda3d5d998b9a1
fai7h01 <l.varsimashvil10@gmail.com>
2024-04-02 20:02:38 +0400

    Map

---
+++ dsa-03-Collections/src/main/java/MapReview.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapReview {
+    public static void main(String[] args) {
+
+        // create hash map
+        Map<Integer, String> studentsMap = new HashMap<>();
+        studentsMap.put(1, "Jack");
+        studentsMap.put(2, "Julian");
+
+
+    }
+
+    public static Character findFirstNonRepeatingChar(String str){
+
+        return null;
+    }
+}
