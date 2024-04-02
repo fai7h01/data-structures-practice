b20b2450cf45237f768e54c87c64d390a57cec04
fai7h01 <l.varsimashvil10@gmail.com>
2024-04-02 19:17:32 +0400

    Set

---
+++ dsa-03-Collections/src/main/java/SetReview.java
@@ -0,0 +1,34 @@
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class SetReview {
+    public static void main(String[] args) {
+
+        //1. create a set
+        Set<Student> set = new LinkedHashSet<>();
+
+        set.add(new Student(1,"Melissa"));
+        set.add(new Student(2,"Julian"));
+        set.add(new Student(3,"Selena"));
+        set.add(new Student(4,"Yin"));
+        System.out.println(set.add(new Student(4, "Yin")));
+
+        System.out.println(set);
+
+        System.out.println(firstRepeatingChar("Java Developer"));
+
+
+    }
+
+    public static Character firstRepeatingChar(String str){
+        Set<Character> set = new HashSet<>();
+
+        for (char ch :str.toCharArray()){
+            if (!set.add(ch)) return ch;
+        }
+
+        return null;
+    }
+
+}
