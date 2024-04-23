79bb0ae4e357e832c77b96a38ada358c34da0a4b
fai7h01 <l.varsimashvil10@gmail.com>
2024-04-23 18:06:03 +0400

    LinkedList

---
+++ dsa-04-LinkedLists/src/main/java/MySinglyLinkedList.java
@@ -0,0 +1,70 @@
+public class MySinglyLinkedList {
+
+    Node head;
+    Node tail;
+    int size;
+
+    boolean isEmpty() {
+        return head == null;
+    }
+
+    void add(int value) {
+        //create a new node object from data
+        Node node = new Node(value);
+        if (isEmpty()) { // if the link is empty
+            head = tail = node;
+            size++;
+        } else { // if there are elements in list
+            tail.next = node;
+            tail = node;
+            size++;
+        }
+    }
+
+    void deleteById(int id) {
+        // check if empty
+        if (isEmpty()) System.out.println("List is empty.");
+
+        Node prev = head;
+        Node current = head;
+
+        while (current != null) {
+            if (current.id == id) { // if there is a match
+                // case 1: head
+                if (current == head) {
+                    head = current.next;
+                    current.next = null;
+                }
+                // case 2: tail
+                else if (current == tail) {
+                    tail = prev;
+                    prev.next = null; // Ex - tail will be eligible for garbage collection
+                }
+                // case 3: middle
+                else {
+                    prev.next = current.next;
+                    current.next = null;
+                }
+                // after deletion
+                size--;
+            }
+            // move forward on the elements of the list
+            prev = current;
+            current = current.next;
+        }
+
+
+    }
+
+    void printNodes() {
+        Node current = head;
+        while (current != null) {
+            if (current.next == null)
+                System.out.println(current.id + " => null");
+            else
+                System.out.print(current.id + " => ");
+            current = current.next;
+        }
+    }
+
+}
---
+++ dsa-04-LinkedLists/src/main/java/SinglyLinkedListApp.java
@@ -0,0 +1,20 @@
+public class SinglyLinkedListApp {
+
+    public static void main(String[] args) {
+
+        MySinglyLinkedList linkedList = new MySinglyLinkedList();
+
+        System.out.println(linkedList.isEmpty());
+
+        for (int i = 0; i < 10; i++) {
+            linkedList.add(i);
+        }
+
+        linkedList.printNodes();
+
+        linkedList.deleteById(7);
+
+        linkedList.printNodes();
+
+    }
+}
