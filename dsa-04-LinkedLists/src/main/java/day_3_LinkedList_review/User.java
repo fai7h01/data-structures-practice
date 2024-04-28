b8138a3855d8360b18d06016536e1f354f5ad17c
fai7h01 <l.varsimashvil10@gmail.com>
2024-04-28 19:35:08 +0400

    LinkedList review

---
+++ dsa-04-LinkedLists/src/main/java/day_3_LinkedList_review/User.java
@@ -0,0 +1,12 @@
+package day_3_LinkedList_review;
+
+public class User {
+    int id;
+    String name;
+    User next;
+
+    public User(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
