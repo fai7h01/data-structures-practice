1f17a025b25fa5215c85f90587ea699bd3c38bf6
fai7h01 <l.varsimashvil10@gmail.com>
2024-05-29 13:28:55 +0400

    stack practice

---
+++ dsa-05-Stack/src/main/java/ExpBalanceCheck.java
@@ -0,0 +1,37 @@
+public class ExpBalanceCheck {
+    public static void main(String[] args) {
+        System.out.println(balanceCheck("()}"));
+    }
+
+    static boolean balanceCheck(String expr){
+        // create a stack
+        MyStack<Character> myStack = new MyStack<>();
+        // iterate over expr
+        for (int i = 0; i < expr.length(); i++) {
+            Character ch = expr.charAt(i);
+            //filter non player chars
+            if (ch != '(' && ch != '[' && ch != '{' && ch != '}' && ch != ']' && ch != ')'){
+                continue;
+            }
+            if (ch == '(' || ch == '[' || ch == '{'){
+                myStack.push(ch);
+                continue;
+            }
+            if (myStack.isEmpty()) return false;
+            switch (ch){
+                case ')':
+                    if (myStack.pop() != '(') return false;
+                    break;
+                case ']':
+                    if (myStack.pop() != '[') return false;
+                    break;
+                case '}':
+                    if (myStack.pop() != '{') return false;
+                    break;
+            }
+        }
+        // return if stack is empty
+        return myStack.isEmpty();
+    }
+
+}
