36722bb93c01b057c6ade27848afe71a0f10f5ae
fai7h01 <l.varsimashvil10@gmail.com>
2024-06-02 18:39:35 +0400

    sunsetView

--- dsa-05-Stack/src/main/java/SunsetView.java
+++ dsa-05-Stack/src/main/java/SunsetView.java
@@ -1,18 +1,41 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Stack;
 
 public class SunsetView {
 
     public static void main(String[] args) {
 
         int[] buildings = {3,5,4,4,3,1,3,2};
-        String direction = "east";
-        System.out.println(sunsetView(buildings, direction));
+        System.out.println(sunsetView(buildings, "east"));
+
     }
 
     static ArrayList<Integer> sunsetView(int[] buildings, String direction){
+        Stack<Integer> stack = new Stack<>();
 
+        //this part is for east by default
+        int index = 0;
+        int step = 1;
+        //this part is for west
+        if (direction.equalsIgnoreCase("west")){
+            index = buildings.length - 1;
+            step = -1;
+        }
 
-        return new ArrayList<>();
+        while (index >= 0 && index < buildings.length) {
+            while (!stack.isEmpty() && buildings[index] >= buildings[stack.peek()]) {
+                stack.pop();
+            }
+            stack.push(index);
+            index += step;
+        }
+        //now i have a stack with building indices that can see sunset
+        if (direction.equalsIgnoreCase("west")){
+            Collections.reverse(stack);
+        }
+
+        return new ArrayList<>(stack);
     }
 
 }
