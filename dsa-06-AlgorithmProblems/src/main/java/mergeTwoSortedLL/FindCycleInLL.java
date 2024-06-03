293542be2298aad2b22785055b6c1d500da48761
fai7h01 <l.varsimashvil10@gmail.com>
2024-06-03 15:22:21 +0400

    FindCycleInLL

---
+++ dsa-06-AlgorithmProblems/src/main/java/mergeTwoSortedLL/FindCycleInLL.java
@@ -0,0 +1,36 @@
+package mergeTwoSortedLL;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class FindCycleInLL {
+
+    public static void main(String[] args) {
+        Node node1 = new Node(1);
+        Node node2 = new Node(2);
+        Node node3 = new Node(3);
+        Node node4 = new Node(4);
+
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node2;
+
+        System.out.println(isCycle(node1));
+    }
+
+    public static boolean isCycle(Node head){
+        Set<Node> set = new HashSet<>();
+
+        Node current = head;
+        while(current != null){
+            if (set.contains(current)) return true;
+            set.add(current);
+            current = current.next;
+        }
+        return false;
+
+    }
+
+
+}
---
+++ dsa-06-AlgorithmProblems/src/main/java/mergeTwoSortedLL/Node.java
@@ -0,0 +1,10 @@
+package mergeTwoSortedLL;
+
+public class Node {
+    int id;
+    Node next;
+
+    public Node(int id){
+        this.id = id;
+    }
+}
