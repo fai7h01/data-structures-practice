f2a1ef7a99668c67afa1449d623505a16e119d0a
fai7h01 <l.varsimashvil10@gmail.com>
2024-06-07 21:29:05 +0400

    Queues

---
+++ dsa-07-Queues/src/main/java/CollectionsQueueTasks.java
@@ -0,0 +1,8 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class CollectionsQueueTasks {
+    public static void main(String[] args) {
+        Deque<Integer> deque = new ArrayDeque<>();
+    }
+}
