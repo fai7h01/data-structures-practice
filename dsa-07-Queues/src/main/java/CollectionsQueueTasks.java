1f9b472b853ab89cef1cf422db1aae52c19edda6
fai7h01 <l.varsimashvil10@gmail.com>
2024-06-07 21:35:57 +0400

    Queues

--- dsa-07-Queues/src/main/java/CollectionsQueueTasks.java
+++ dsa-07-Queues/src/main/java/CollectionsQueueTasks.java
@@ -1,5 +1,6 @@
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.PriorityQueue;
 
 public class CollectionsQueueTasks {
     public static void main(String[] args) {
---
+++ dsa-07-Queues/src/main/java/MyQueue.java
@@ -0,0 +1,27 @@
+public class MyQueue<T> {
+
+    QNode<T> front;
+    QNode<T> back;
+    int size;
+
+    boolean isEmpty() {
+        return front == null;
+    }
+
+    T peek(){
+        return front.value;
+    }
+
+    void enqueue(T item){
+        QNode<T> node = new QNode<>(item);
+        if (isEmpty()){
+            front = back = node;
+        }else{
+            back.next = node;
+            back = node;
+        }
+        size++;
+    }
+
+
+}
