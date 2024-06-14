ff12bb1b5c1aaf1f57411f190558867219821784
fai7h01 <l.varsimashvil10@gmail.com>
2024-06-14 20:24:31 +0400

    queues and hashing review

--- dsa-07-Queues/src/main/java/queuesAndHashingReview/MyHashMapTestApp.java
+++ dsa-07-Queues/src/main/java/queuesAndHashingReview/MyHashMapTestApp.java
@@ -1,4 +1,15 @@
 package queuesAndHashingReview;
 
 public class MyHashMapTestApp {
+    public static void main(String[] args) {
+
+        MyHashMap map = new MyHashMap();
+        map.put(1, 10);
+        map.put(2, 20);
+        map.put(3, 30);
+        map.put(4, 40);
+        System.out.println(map.get(1));
+        System.out.println(map.get(4));
+
+    }
 }
