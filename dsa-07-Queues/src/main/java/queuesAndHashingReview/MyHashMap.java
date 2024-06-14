6895a6b6a3c4399cb8536cdba587e130af1e1635
fai7h01 <l.varsimashvil10@gmail.com>
2024-06-14 20:05:25 +0400

    queues and hashing review

--- dsa-07-Queues/src/main/java/queuesAndHashingReview/MyHashMap.java
+++ dsa-07-Queues/src/main/java/queuesAndHashingReview/MyHashMap.java
@@ -1,19 +1,36 @@
 package queuesAndHashingReview;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyHashMap {
 
+    List<BucketLinkedList> list;
+    int modulusNumber;
+
+    public MyHashMap(){
+        this.list = new ArrayList<>();
+        this.modulusNumber = 2003;
+        for (int i = 0; i < 2003; i++) {
+            this.list.add(new BucketLinkedList());
+        }
+    }
 
 
     public void put(int key, int value){
-
+        //calculate index from key with modulus
+        int index = key % modulusNumber;
+        list.get(index).put(key, value);
     }
 
     public int get(int key){
-        return -1;
+        int index = key % modulusNumber;
+        return list.get(index).get(key);
     }
 
     public void remove(int key){
-
+        int index = key % modulusNumber;
+        list.get(index).remove(key);
     }
 
 }
