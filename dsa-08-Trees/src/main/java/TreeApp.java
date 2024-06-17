1951c9230186caa127e8626f245af8b0a0ab40db
fai7h01 <l.varsimashvil10@gmail.com>
2024-06-17 23:04:57 +0400

    Trees part 1

---
+++ dsa-08-Trees/src/main/java/TreeApp.java
@@ -0,0 +1,15 @@
+public class TreeApp {
+    public static void main(String[] args) {
+
+        MyTree tree = new MyTree();
+
+        int[] nums = {10, 6, 8, 20, 4, 9, 5, 17, 42, 47, 29};
+
+        for (int i = 0; i < nums.length; i++) {
+            tree.insert(nums[i]);
+        }
+
+        VisualizeTree.printTree(tree.root,null,false);
+
+    }
+}
