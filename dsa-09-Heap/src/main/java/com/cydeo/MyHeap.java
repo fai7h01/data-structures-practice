89e7233121ac2f79bc30b26b0a00bcc9de37c7c9
fai7h01 <l.varsimashvil10@gmail.com>
2024-07-05 17:36:10 +0400

    Heap

--- dsa-09-Heap/src/main/java/com/cydeo/MyHeap.java
+++ dsa-09-Heap/src/main/java/com/cydeo/MyHeap.java
@@ -5,21 +5,45 @@ public class MyHeap {
     int[] items;
     int size;
 
-    public MyHeap(int capacity){
+    public MyHeap(int capacity) {
         this.items = new int[capacity];
         this.size = 0;
     }
 
-    public int leftChildIndex(int index){
+    public int leftChildIndex(int index) {
         return index * 2 + 1;
     }
 
-    public int rightChildIndex(int index){
+    public int rightChildIndex(int index) {
         return index * 2 + 2;
     }
 
-    public int parentIndex(int index){
+    public int parentIndex(int index) {
         return (index - 1) / 2;
     }
 
+    public boolean hasLeftChild(int index) {
+        return leftChildIndex(index) < this.size;
+    }
+
+    public boolean hasRightChild(int index) {
+        return rightChildIndex(index) < this.size;
+    }
+
+    public int largerChildIndex(int index) {
+        // no child
+        if (!hasLeftChild(index)) {
+            return index;
+        }
+        // one child - left child
+        else if (!hasRightChild(index)) {
+            return leftChildIndex(index);
+        } else {
+            if (items[leftChildIndex(index)] > items[rightChildIndex(index)]){
+                return leftChildIndex(index);
+            }else{
+                return rightChildIndex(index);
+            }
+        }
+    }
 }
