a862a5a97bd3fe90a11572b85fde84ca973e4e47
fai7h01 <l.varsimashvil10@gmail.com>
2024-07-15 19:28:32 +0400

    Search algorithms

--- dsa-10-Search/src/main/java/TernarySearch.java
+++ dsa-10-Search/src/main/java/TernarySearch.java
@@ -5,29 +5,30 @@ public class TernarySearch {
         int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         int data = 6;
 
-        System.out.println(ternarySearch(numbers,data));
+        System.out.println(ternarySearch(numbers, data));
+        System.out.println(ternarySearchRecursive(numbers, data, 0, numbers.length - 1));
 
     }
 
-    public static int ternarySearch(int[] array, int data){
+    public static int ternarySearch(int[] array, int data) {
         int left = 0;
         int right = array.length - 1;
 
-        while (left <= right){
+        while (left <= right) {
             int partition = (right - left) / 2;
             int mid1 = left + partition;
             int mid2 = right - partition;
-            if (array[mid1] == data){
+            if (array[mid1] == data) {
                 return mid1;
             }
-            if (array[mid2] == data){
+            if (array[mid2] == data) {
                 return mid2;
             }
-            if (data < array[mid1]){
+            if (data < array[mid1]) {
                 right = mid1 - 1;
-            }else if (data > array[mid2]){
+            } else if (data > array[mid2]) {
                 left = mid2 + 1;
-            }else{
+            } else {
                 left = mid1 + 1;
                 right = mid2 - 1;
             }
@@ -35,24 +36,24 @@ public class TernarySearch {
         return -1;
     }
 
-    public static int ternarySearchRecursive(int[] array, int data, int left, int right){
-        if (left > right){
+    public static int ternarySearchRecursive(int[] array, int data, int left, int right) {
+        if (left > right) {
             return -1;
         }
         int partition = (right - left) / 2;
         int mid1 = left + partition;
         int mid2 = right - partition;
-        if (array[mid1] == data){
+        if (array[mid1] == data) {
             return mid1;
         }
-        if (array[mid2] == data){
+        if (array[mid2] == data) {
             return mid2;
         }
-        if (data < array[mid1]){
+        if (data < array[mid1]) {
             return ternarySearchRecursive(array, data, left, mid1 - 1);
-        }else if (data > array[mid2]){
+        } else if (data > array[mid2]) {
             return ternarySearchRecursive(array, data, mid2 + 1, right);
-        }else{
+        } else {
             return ternarySearchRecursive(array, data, mid1 + 1, mid2 - 1);
         }
     }
